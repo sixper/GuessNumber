91f2c607fb906cc8a5b16562f666ce4ebc658c82
fsixper <fisxper@gmail.com>
2022-01-10 20:54:58 +0300

    last

--- src/Solution/Calculator.java
+++ src/Solution/Calculator.java
@@ -10,47 +10,18 @@ public class Calculator {
 
         Scanner input = new Scanner(System.in);
 
-        double math,physic,turkish,chemistry,music;
-        double average = 0;
+        System.out.print("Lütfen hava sıcaklığını giriniz: ");
 
-        System.out.print("Matematik notunuzu giriniz: ");
-        math = input.nextInt();
+        int temperature = input.nextInt();
 
-        System.out.print("Fizik notunuzu giriniz: ");
-        physic = input.nextInt();
-
-        System.out.print("Türkçe notunuzu giriniz: ");
-        turkish = input.nextInt();
-
-        System.out.print("Kimya notunuzu giriniz: ");
-        chemistry = input.nextInt();
-
-        System.out.print("Müzik notunuzu giriniz: ");
-        music = input.nextInt();
-
-        if(math >= 0 && math <= 100)
-            average += math;
-
-        if(physic >= 0 && physic <= 100)
-            average += physic;
-
-        if(turkish >= 0 && turkish <= 100)
-            average += turkish;
-
-        if(chemistry >= 0 && chemistry <= 100)
-            average += chemistry;
-
-        if(music >= 0 && music <= 100)
-            average += music;
-
-        average /= 5;
-
-        System.out.println("Not ortalamanız: " + average);
-
-        if(average >= 55)
-            System.out.println("Tebrikler sınıfı geçtiniz.");
+        if(temperature < 5){
+            System.out.println("Go kayak");
+        }else if(temperature >= 5 && temperature < 15)
+            System.out.println("Sinema zamanı");
+        else if(temperature >= 15 && temperature < 25)
+            System.out.println("Piknik <3");
         else
-            System.out.println("Sınıfta kaldınız");
+            System.out.println("Hadi yüzelim");
 
 
 
