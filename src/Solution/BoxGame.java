ad0f589d65bd68a73c5acc0d1eac0dda4c4750c4
fsixper <fisxper@gmail.com>
2022-01-20 00:43:12 +0300

    last

---
+++ src/Solution/BoxGame.java
@@ -0,0 +1,25 @@
+package Solution;
+
+import java.util.*;
+
+public class BoxGame {
+
+
+
+    public static void main(String[] args) {
+
+        Fighter marc = new Fighter("Marc" , 10 , 100, 90, 0);
+        Fighter alex = new Fighter("Alex" , 10 , 100, 100, 0);
+
+        Ring r = new Ring(marc,alex , 90 , 100);
+        r.run();
+
+    }
+
+
+
+
+}
+
+
+
